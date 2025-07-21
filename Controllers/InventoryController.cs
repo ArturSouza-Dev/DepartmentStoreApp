c4fc989566453a9f1f9e4d29f2456b853743a725
Artur Souza <artur95gss@gmail.com>
2025-07-20 20:28:10 -0400

    Navigation bar created for Inventory/Index

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,40 @@
+﻿using DepartmentStoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DepartmentStoreApp.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private readonly DepartmentStoreDbContext _dbContext;
+
+        public InventoryController(DepartmentStoreDbContext context) 
+        { 
+            _dbContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public JsonResult GetInventoryData()
+        {
+            var inventoryData = _dbContext.Inventories.ToList();
+            return Json(inventoryData);
+        }
+
+        [HttpGet]
+        public ActionResult LoadInventoryOrderLines()
+        {
+            var model = _dbContext.OrderLines.ToList();
+            return PartialView("_OrderLines", model);
+        }
+
+        [HttpGet]
+        public ActionResult LoadInventoryShipmentLines()
+        {
+            var model = _dbContext.ShipmentLines.ToList();
+            return PartialView("_ShipmentLines", model);
+        }
+    }
+}
